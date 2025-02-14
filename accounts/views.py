526fc88b6a63e46ada5c8a7fd33fffc98487ea06
manyworldss <Ralphsuber@gmail.com>
2025-02-14 00:57:19 -0500

    Design changes (light, sleek)

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
 
 class CustomUserCreationForm(UserCreationForm):
     # This creates a new form class that inherits from Django's built-in UserCreationForm
@@ -24,8 +25,19 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('core:home')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Successfully logged in!')
+            return redirect('core:home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
--- emergency_cards/urls.py
+++ emergency_cards/urls.py
@@ -6,5 +6,6 @@ app_name = 'emergency_cards'
 urlpatterns = [
     path('create/', views.create_card, name='create_card'),
     path('card/<int:card_id>/', views.card_detail, name='card_detail'),
+    path('validate-field/', views.validate_field, name='validate_field'),
 ]
 
