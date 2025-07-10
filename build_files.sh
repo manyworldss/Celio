978a3143893b4e0ea51728bdc3e7a586c794ddd9
Madvillain <Ralphsuber@gmail.com>
2025-07-10 14:33:10 -0400

    new build

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Starting build process..."
 
-# Ensure python3.9 is available
+# Ensure python3.11 is available
 if ! command -v python3.9 &> /dev/null
 then
     echo "python3.9 could not be found"
@@ -15,15 +15,15 @@ echo "Found python3.9"
 echo "Path to python3.9: $(which python3.9)"
 
 # Ensure pip is installed
-python3.9 -m ensurepip --upgrade
+python3.11 -m ensurepip --upgrade
 
 echo "Installing dependencies from requirements.txt..."
-python3.9 -m pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
 
 echo "Dependencies installed successfully. Listing installed packages:"
-python3.9 -m pip freeze
+python3.11 -m pip freeze
 
 echo "Running collectstatic..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.11 manage.py collectstatic --noinput --clear
 
 echo "collectstatic finished successfully."
